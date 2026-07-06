b17b09529b9e449d68b2bb8d65de3a88cb9180fd
eliaheidrich03-cloud <eliaheidrich03@gmail.com>
2026-07-06 12:49:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "sg-nowo-v13";
+const CACHE = "sg-nowo-v27";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
